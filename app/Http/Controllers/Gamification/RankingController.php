373bd84fa929928b0b171facc7f3c688efbc6290
danielhe4rt <danielhe4rt@gmail.com>
2022-12-24 18:21:32 -0300

    fix: ranking

--- app/Http/Controllers/Gamification/RankingController.php
+++ app/Http/Controllers/Gamification/RankingController.php
@@ -4,13 +4,11 @@ namespace App\Http\Controllers\Gamification;
 
 use App\Http\Controllers\Controller;
 use App\Repositories\Gamification\RankingRepository;
-use App\Traits\ApiResponse;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class RankingController extends Controller
 {
-    use ApiResponse;
-
     /**
      * @var RankingRepository
      */
@@ -21,68 +19,15 @@ class RankingController extends Controller
         $this->repository = $repository;
     }
 
-    /**
-     * Retrieve Ranking by Level/Exp
-     * @param Request $request
-     * @author danielhe4rt - hey@danielheart.dev
-     * @param Request $request
-     * @return JsonResponse
-     * @OA\Get(
-     *     path="/ranking/general",
-     *     summary="Retorna o ranking de Level/exp global",
-     *     operationId="getRankingGeneral",
-     *     tags={"ranking"},
-     *     @OA\Parameter(
-     *         name="type",
-     *         in="path",
-     *         description="Nem existe ainda mas vo deixa documentado",
-     *         required=false,
-     *         @OA\Schema(
-     *           type="string",
-     *         )
-     *     ),
-     *     @OA\Response(
-     *         response=200,
-     *         description="...",
-     *     )
-     * )
-     */
-
-    public function getGeneralLevelRanking(Request $request)
+    public function getGeneralLevelRanking(Request $request): JsonResponse
     {
         $result = $this->repository->rankingByLevel($request->all());
-        return $this->success($result);
+        return response()->json($result);
     }
 
-    /**
-     * Quick description about the function
-     * @author danielhe4rt - hey@danielheart.dev
-     * @param Request $request
-     *
-     * @return JsonResponse
-     * @OA\Get(
-     *     path="/ranking/messages",
-     *     summary="Retorna o ranking de Mensagens",
-     *     operationId="getRankingMessages",
-     *     tags={"ranking"},
-     *     @OA\Parameter(
-     *         name="type",
-     *         in="path",
-     *         description="week = ultima semana | month = ultimo mes",
-     *         required=false,
-     *         @OA\Schema(
-     *           type="string",
-     *         )
-     *     ),
-     *     @OA\Response(
-     *         response=200,
-     *         description="...",
-     *     )
-     * )
-     */
-    public function getGeneralMessageRanking(Request $request)
+    public function getGeneralMessageRanking(Request $request): JsonResponse
     {
         $result = $this->repository->rankingByMessages($request->all());
-        return $this->success($result);
+        return response()->json($result);
     }
 }
