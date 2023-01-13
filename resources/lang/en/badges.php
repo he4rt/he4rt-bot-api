6274e9ce195df6828ff5f52d834b1a234051e93e
danielhe4rt <hey@danielheart.dev>
2023-01-13 18:52:20 -0300

    feat(init): he4rtbot shippuden

--- resources/lang/en/badges.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'success' => 'Você adquiriu a insignia *:badgeName*. Congratz!',
-    'errors' => [
-        'alreadyClaimed' => 'Você já adquiriu essa insígnia. Consulte seu perfil para mais informações',
-        'inactive' => 'Insignia inativa! Aguarde o momento para resgatá-la.',
-    ]
-];
