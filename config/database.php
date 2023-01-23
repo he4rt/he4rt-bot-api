568e193c9eadee50e833ca7a717ef1ee6f213be7
Rafael Neris <rafael.cruz@picpay.com>
2023-01-22 22:26:30 -0300

    Default database host

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'db'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
