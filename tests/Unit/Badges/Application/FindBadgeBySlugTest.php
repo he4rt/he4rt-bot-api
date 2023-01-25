73328329d249656486c5ff6016e6bf480bbc20a5
Canhassi12 <arthurcanhassi@gmail.com>
2023-01-25 01:38:25 -0300

    test: badge actions and application unit test

---
+++ tests/Unit/Badges/Application/FindBadgeBySlugTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Unit\Badges\Application;
+
+use Heart\Badges\Application\FindBadgeBySlug;
+use Heart\Badges\Domain\Entities\BadgeEntity;
+use Heart\Badges\Domain\Repositories\BadgeRepository;
+use Mockery\MockInterface;
+use Mockery as m;
+use Tests\TestCase;
+use Tests\Unit\Badges\BadgeProviderTrait;
+
+class FindBadgeBySlugTest extends TestCase
+{
+    use BadgeProviderTrait;
+
+    private MockInterface $badgeRepositoryStub;
+
+    private BadgeEntity $badgeEntity;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->badgeRepositoryStub = m::mock(BadgeRepository::class);
+        $this->badgeEntity = $this->validBadgeEntity();
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+        m::close();
+    }
+
+    public function testFindBadgeBySlug(): void
+    {
+        $this->badgeRepositoryStub
+            ->shouldReceive('findBySlug')
+            ->with("é o canhas")
+            ->once()
+            ->andReturn($this->badgeEntity);
+
+        $test = new FindBadgeBySlug($this->badgeRepositoryStub);
+
+        $test->handle("é o canhas");
+    }
+}
