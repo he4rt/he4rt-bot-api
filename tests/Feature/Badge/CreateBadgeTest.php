cd3d93d783e396d5d5b37a611bcb08169db4e1ac
danielhe4rt <hey@danielheart.dev>
2023-01-28 15:26:36 -0300

    wip

--- tests/Feature/Badge/CreateBadgeTest.php
+++ tests/Feature/Badge/CreateBadgeTest.php
@@ -3,12 +3,13 @@
 namespace Tests\Feature\Badge;
 
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 
 class CreateBadgeTest extends TestCase
 {
-    use DatabaseTransactions;
+    use RefreshDatabase;
 
     public function testCanCreateBadge()
     {
