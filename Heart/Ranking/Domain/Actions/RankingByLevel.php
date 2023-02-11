ad51fcfb1ad4c4bb0428ba64970db29cdb833fad
danielhe4rt <hey@danielheart.dev>
2023-02-10 22:41:08 -0300

    wip: return type

--- Heart/Ranking/Domain/Actions/RankingByLevel.php
+++ Heart/Ranking/Domain/Actions/RankingByLevel.php
@@ -3,6 +3,7 @@
 namespace Heart\Ranking\Domain\Actions;
 
 use Heart\Ranking\Domain\Repositories\RankingRepository;
+use Heart\Shared\Domain\Paginator;
 
 class RankingByLevel
 {
@@ -11,7 +12,7 @@ class RankingByLevel
     ) {
     }
 
-    public function handle()
+    public function handle(): Paginator
     {
         return $this->rankingRepository->rankingByLevel();
     }
