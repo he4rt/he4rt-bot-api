5987be74cd7128e81f7d275277feb30b549486a6
Canhassi <arthurcanhassi@gmail.com>
2023-08-26 20:17:37 -0300

    chore: add return type in migrations

--- database/migrations/2023_08_26_175456_create_team_roles_table.php
+++ database/migrations/2023_08_26_175456_create_team_roles_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('team_roles', function (Blueprint $table) {
             $table->id();
@@ -26,7 +26,7 @@ return new class extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('team_roles');
     }
