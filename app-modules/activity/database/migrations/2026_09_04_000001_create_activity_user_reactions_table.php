f25a87b5e8f84843abcbcab86980bb888b7253b1
GabrielFV <gabrielvasco906@gmail.com>
2026-09-08 21:29:31 -0300

    feat(reaction): update reaction column comment to use TimelineReaction enum

--- app-modules/activity/database/migrations/2026_09_04_000001_create_activity_user_reactions_table.php
+++ app-modules/activity/database/migrations/2026_09_04_000001_create_activity_user_reactions_table.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use He4rt\Activity\Reaction\Enums\TimelineReaction;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,8 +15,7 @@ return new class extends Migration
             $table->uuid('id')->primary();
             $table->foreignUuid('user_id')->constrained('users');
             $table->foreignUuid('timeline_id')->constrained('activity_timeline')->cascadeOnDelete();
-            // TODO(#540): trocar pelo comment gerado por TimelineReaction::stringifyCases() quando o enum existir.
-            $table->string('reaction')->comment('like|love|laugh|celebrate|fire|sad');
+            $table->string('reaction')->comment(TimelineReaction::stringifyCases());
             $table->timestampsTz();
 
             $table->unique(['user_id', 'timeline_id'], 'activity_user_reactions_user_timeline_unique');
